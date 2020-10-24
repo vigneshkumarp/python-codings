d67cf46ee025cfa3e9166e32c15db6b31a788a33
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2020-10-23 19:50:50 -0700

    Update list_sort.py

--- list_sort.py
+++ list_sort.py
@@ -2,9 +2,8 @@ n = int(input("enter:"))
 l=[]
 for x in range(n):
     i = int(input())
-    if i < 6:
-     l.append(i)
-     l.sort()
+    l.append(i)
+    l.sort()
 if 4 in l:
     l.remove(4)
     print(l[-1])
